f4c394dffe104fe3a45ad2ae034da718d66558e4
ali85k <aliceanndraad56@gmail.com>
2022-06-30 22:03:32 -0300

    first-commit

---
+++ script.js
@@ -0,0 +1,35 @@
+if(idade >= 18){
+    console.log("Você  e obrigado a votar")
+ }else{
+     console.log("Você pode beber")
+}
+
+let girl= false
+if(idade >= 18 && girl=== true){
+     console.log("Você é uma garota legal")
+}else{
+     console.log("Você é uma boa pessoa")
+}
+
+let mes = "outubro"
+
+if(mes === "Janeiro" || mes === "Dezembro"){
+    console.log("Parabéns!")
+}else{
+    console.log("Não é teu niver" Alice!")
+}
+
+ let nome = "Alice"
+if(nome[0] === "R"){
+     console.log("Interessante")
+}else{
+     console.log("Não e tão interessante")
+ }
+
+let sobrenome = "Andrade"
+
+if(sobrenome.length >= 6 || nome[0] === "E"){
+   console.log("isso e legal")
+}else{
+    console.log("Isso não tão legal")
+ }
